9ae52c030c2dd5f9c9384dafe62773c17cb68e91
Kaden Shorf and Steve Spitz <kaden@>
2017-05-31 10:03:49 -0700

    add additional card information correct bug in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
   //Initial hides//
   $("#janswer").hide();
   $("#oanswer").hide();
-$(".card1").click(function(){
+  $("#vanswer").hide();
+$(".card1").click(function() {
   $("#jcard").toggle();
   $("#janswer").toggle();
-});
-$(".card2").click(function(){
+  });
+$(".card2").click(function() {
   $("#ocard").toggle();
   $("#oanswer").toggle();
-});
+  });
+$(".card3").click(function() {
+  $("#vcard").toggle();
+  $("#vanswer").toggle();
+  });
 });
